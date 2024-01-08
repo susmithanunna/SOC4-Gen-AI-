e082ec3ff54b2acef2606be7163f09965638ec8f
Susmitha Radha Devi Nunna <126801279+susmithanunna@users.noreply.github.com>
2024-01-08 11:56:32 +0530

    1.py

--- 1.py
+++ 1.py
@@ -1,7 +1,7 @@
 import streamlit as st #streamlit is package in python used to create  UI
 st.set_page_config(page_title='Foodie')
 st.header("Tyoes of Foodies")
-col1,col2-st.columns(2)
+col1,col2=st.columns(2)
 with col1:
   st.subheader("Shinchan ")
   st.image("https://github.com/susmithanunna/SOC4-Gen-AI-/blob/main/hungry2.gif",caption="Cute Eater",width=300,use_column_width=True)
