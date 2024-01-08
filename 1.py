5a051fc23dd89ce44c87cc9ade5d15ea91ebf506
Susmitha Radha Devi Nunna <126801279+susmithanunna@users.noreply.github.com>
2024-01-08 12:04:00 +0530

    1.py

--- 1.py
+++ 1.py
@@ -4,10 +4,10 @@ st.header("Tyoes of Foodies")
 col1,col2=st.columns(2)
 with col1:
   st.subheader("Shinchan ")
-  st.image("https://media4.giphy.com/media/12Yug01me26oH6/giphy.gif",caption="Cute Eater",width=300,height=300,use_column_width=True)
+  st.image("https://media4.giphy.com/media/12Yug01me26oH6/giphy.gif",caption="Cute Eater",width=300,use_column_width=True)
   st.write("Cute Eater Shinchana")
 with col2:
     st.subheader("Duck")
-    st.image("https://media.tenor.com/AFagQ7fK8JUAAAAj/food-yummy.gif",caption="Fast Eater", width=300,height=300,use_column_width=True)
+    st.image("https://media.tenor.com/AFagQ7fK8JUAAAAj/food-yummy.gif",caption="Fast Eater", width=300,use_column_width=True)
     st.write("Fast eater Duck")
     
